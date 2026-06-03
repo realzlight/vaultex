efb5c3e6c287ddb922d7fb446fcc83c1900e4a27
realzlight <legitzlight@gmail.com>
2026-06-03 19:56:58 +0530

    clean slate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Signin from './pages/Signin'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signin" element={<Signin />} />
-        </Routes>
-      </div>
+      <Routes>
+      </Routes>
     </BrowserRouter>
   )
 }
