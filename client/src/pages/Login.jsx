53b24d60c1b2e2ddcec5c7a56681da9bdb73c305
realzlight <legitzlight@gmail.com>
2026-06-03 20:49:51 +0530

    update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,30 +2,21 @@ import React from 'react'
 import './Login.css'
 import AuthUI from '../components/AuthUI'
 
-function Login(props){
-  
-  function Login() {
+function Login() {
   const handleSubmit = () => {
     console.log("Submit clicked")
   }
-  }
-  
+
   return (
     <div className="LoginContainer">
       <AuthUI
-      title="Log in to your account"
-      buttonText="Continue"
-      footerText="Don't have an account?"
-      footerLink="/signup"
-      footerLinkText="Create your account"
-      onSubmit={handleSubmit}
-    />
-      
-      
-      
-      
-      
-      
+        title="Log in to your account"
+        buttonText="Continue"
+        footerText="Don't have an account?"
+        footerLink="/signup"
+        footerLinkText="Create your account"
+        onSubmit={handleSubmit}
+      />
     </div>
   )
 }
