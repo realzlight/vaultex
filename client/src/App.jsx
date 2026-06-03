8fb759827312a739859efa74152cb6a97b47b1eb
realzlight <legitzlight@gmail.com>
2026-06-03 13:53:33 +0530

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from 'react'
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signin from './pages/Signin'
-
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-     <Routes>
-       
-     <Route path="/login" element={<Login/>}/>
-     <Route path="/signin" element={<Signin/>}/>
-          
-     </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
       </BrowserRouter>
-      
     </div>
   )
 }
