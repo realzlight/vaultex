8f94bab3fa3e964d1fd75252d5cee32402e7b0ff
realzlight <legitzlight@gmail.com>
2026-06-09 16:33:31 +0530

    vaultex api wotking

---
+++ server/middleware/protect.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const protect = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Not authorized, no token' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Token expired or invalid' });
+  }
+};
+
+export default protect;
