c2a69756fbbea25129e94319de9ba84ce4a2d4e3
realzlight <legitzlight@gmail.com>
2026-06-09 17:33:14 +0530

    dashboard workijg with no css

--- server/index.js
+++ server/index.js
@@ -1,39 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import connectDB from './configs/connectDB.js';
 import authRoutes from './routes/auth.js';
-import protect from '../middleware/protect.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors({ 
+app.use(cors({
   origin: 'http://localhost:5173',
-  credentials: true 
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Connect DB
 await connectDB();
 
 app.use('/api/auth', authRoutes);
-app.post('/test', (req, res) => {
-  res.json({ message: 'Test works' });
-});
 
-
-router.get('/verify', protect, (req, res) => {
-  res.status(200).json({ valid: true });
-});
 app.get('/', (req, res) => {
   res.json({ message: 'Server running' });
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,10 @@
-console.log('auth route loaded')
 
 
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import protect from '../middleware/protect.js';
 
 const router = express.Router();
 router.get('/verify', protect, (req, res) => {
